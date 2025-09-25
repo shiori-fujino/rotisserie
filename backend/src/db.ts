3a0a092e2adbc67a907d8b078c686e3f579dd238
juleslovesyou <145836364+juleslovesyou@users.noreply.github.com>
2025-09-25 20:40:29 +1000

    짜증

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,9 +1,8 @@
-// backend/src/db.ts
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // Neon requires SSL
+  connectionString: process.env.DATABASE_URL, // 👈 prefer this
+  ssl: process.env.DATABASE_URL?.includes("railway") ? { rejectUnauthorized: false } : undefined,
 });
 
 export default pool;
